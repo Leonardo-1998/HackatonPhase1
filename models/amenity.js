df4387a2cd6f552e5303cb315e474897f3e5b37b
Alfikri <alfzein19@gmail.com>
2025-04-08 20:40:39 +0700

    CHECKPOINT: Commit before merge with main

--- models/amenity.js
+++ models/amenity.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Amenity.belongsToMany(models.Room,{
-        through: RoomAmenity,
+        through: 'RoomAmenity',
         foreignKey: 'AmenityId',
         otherKey: 'RoomId'
       })
